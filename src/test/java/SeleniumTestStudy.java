805f0b2e124c0d70ac4aad244b6db7c2209b694a
Syamil Hafiy <syamilhca@gmail.com>
2026-05-15 21:49:40 +0800

    Update SeleniumTestStudy.java

--- src/test/java/SeleniumTestStudy.java
+++ src/test/java/SeleniumTestStudy.java
@@ -1,3 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class SeleniumTestStudy extends BaseStructure{
 
    @Test
