b80f1f5bda1957dc2d175875a3e686452b3c77d1
Syamil Hafiy <syamilhca@gmail.com>
2026-05-15 21:47:19 +0800

    Update UltimateQASignUp.java

--- src/main/java/UltimateQASignUp.java
+++ src/main/java/UltimateQASignUp.java
@@ -1,3 +1,7 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class UltimateQASignUp {
 
     private WebDriver driver;
@@ -13,21 +17,49 @@ public class UltimateQASignUp {
     By submitLoc = By.name("et_builder_submit_button");
     By responseLoc = By.cssSelector(".et-pb-contact-message");
 
-    public void fillForm(){
+    public void fillForm(String name, String email, String message, int captchaOffset){
         //Fill Form
         driver.findElement(nameLoc).sendKeys(name);
         driver.findElement(emailLoc).sendKeys(email);
         driver.findElement(messageLoc).sendKeys(message);
 
-        //Dynamic solving of captcha based on the two displayed numbers
+        //Dynamic solving of captcha
         WebElement captcha = driver.findElement(captchaLoc);
         int firstDigit  = Integer.parseInt(captcha.getAttribute("data-first_digit"));
         int secondDigit = Integer.parseInt(captcha.getAttribute("data-second_digit"));
-        int answer      = firstDigit + secondDigit;
+        int answer      = firstDigit + secondDigit + captchaOffset;
         captcha.sendKeys(String.valueOf(answer));
     }
 
-      public void submitButton(){
+    public void checkWrongCaptcha(String name, String email, String message, int captchaOffset){
+        //Fill Form
+        driver.findElement(nameLoc).sendKeys(name);
+        driver.findElement(emailLoc).sendKeys(email);
+        driver.findElement(messageLoc).sendKeys(message);
+
+        //Dynamic solving of captcha
+        WebElement captcha = driver.findElement(captchaLoc);
+        int firstDigit  = Integer.parseInt(captcha.getAttribute("data-first_digit"));
+        int secondDigit = Integer.parseInt(captcha.getAttribute("data-second_digit"));
+        int answer      = firstDigit + secondDigit + captchaOffset;
+        captcha.sendKeys(String.valueOf(answer));
+    }
+
+    public void checkInvalidEmail(String name, String email, String message, int captchaOffset){
+        //Fill Form
+        driver.findElement(nameLoc).sendKeys(name);
+        driver.findElement(emailLoc).sendKeys(email); // Invalid email
+        driver.findElement(messageLoc).sendKeys(message);
+
+        //Dynamic solving of captcha
+        WebElement captcha = driver.findElement(captchaLoc);
+        int firstDigit  = Integer.parseInt(captcha.getAttribute("data-first_digit"));
+        int secondDigit = Integer.parseInt(captcha.getAttribute("data-second_digit"));
+        int answer      = firstDigit + secondDigit + captchaOffset;
+        captcha.sendKeys(String.valueOf(answer));
+    }
+
+    public void submitButton(){
         //Submit Form
         driver.findElement(submitLoc).click();
     }
